ac8c1e624048210b3fc2f93a3bc5b4fe7d6eae06
Kingseungil <109774037+kingseungil@users.noreply.github.com>
2023-11-09 15:18:10 +0900

    Title: 연속되는 수 3, Time: 85ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 231109/연속되는 수 3/continuous-number3.java
@@ -0,0 +1,75 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) {
+        FastReader rd = new FastReader();
+        int n = rd.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = rd.nextInt();
+        }
+
+        int currentCnt = 1;
+        int maxCnt = 0;
+        if (arr.length == 1) {
+            maxCnt = 1;
+        }
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < 0 && arr[i - 1] < 0) {
+                currentCnt++;
+            } else {
+                currentCnt = 1;
+            }
+            maxCnt = Math.max(maxCnt, currentCnt);
+        }
+        System.out.println(maxCnt);
+    }
+
+
+    static class FastReader {
+
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
