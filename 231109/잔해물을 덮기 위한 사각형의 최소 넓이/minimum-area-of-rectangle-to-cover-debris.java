35159286521833e3fdf85b4074bf31a66736ebe8
Kingseungil <109774037+kingseungil@users.noreply.github.com>
2023-11-09 11:36:42 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231109/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.java
+++ 231109/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.java
@@ -14,19 +14,29 @@ public class Main {
         int[] rect1 = {rd.nextInt(), rd.nextInt(), rd.nextInt(), rd.nextInt()};
         int[] rect2 = {rd.nextInt(), rd.nextInt(), rd.nextInt(), rd.nextInt()};
 
-        int x1 = Math.min(rect1[0], rect1[2]);
-        int y1 = Math.min(rect1[1], rect1[3]);
-        int x2 = Math.max(rect1[0], rect1[2]);
-        int y2 = Math.max(rect1[1], rect1[3]);
-
         // rect1이 rect2 안에 있는 경우
-        if (x1 >= rect2[0] && y1 >= rect2[1] && x2 <= rect2[2] && y2 <= rect2[3]) {
-            System.out.println(0);
+        if (rect2[0] <= rect1[0] && rect2[1] <= rect1[1] && rect1[2] <= rect2[2] && rect1[3] <= rect2[3]) {
+            System.out.println((rect1[2] - rect1[0]) * (rect1[3] - rect1[1]));
             return;
         }
+        // rect1넓이 - 겹치는 부분
+        System.out.println((rect1[2] - rect1[0]) * (rect1[3] - rect1[1]) - getOverlapArea(rect1, rect2));
+    }
 
-        System.out.println((x2 - x1) * (y2 - y1));
+    private static int getOverlapArea(int[] rect1, int[] rect2) {
+        int area = 0;
+        for (int i = rect1[0]; i < rect1[2]; i++) {
+            for (int j = rect1[1]; j < rect1[3]; j++) {
+                if (isInRect(i, j, rect2)) {
+                    area++;
+                }
+            }
+        }
+        return area;
+    }
 
+    private static boolean isInRect(int x, int y, int[] rect2) {
+        return rect2[0] <= x && x < rect2[2] && rect2[1] <= y && y < rect2[3];
     }
 
 
