af3eef7f06822e1f73a4410914733030b0d59f01
Kingseungil <109774037+kingseungil@users.noreply.github.com>
2023-11-09 16:08:07 +0900

    Title: T를 초과하는 연속 부분 수열, Time: 101ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 231109/T를 초과하는 연속 부분 수열/subsequence-above-t.java
@@ -0,0 +1,76 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) {
+        FastReader rd = new FastReader();
+        int n = rd.nextInt();
+        int t = rd.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = rd.nextInt();
+        }
+
+        int currentCnt = 0;
+        int maxCnt = -1;
+        if (arr.length == 1) {
+            maxCnt = 1;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > t) {
+                currentCnt++;
+            } else {
+                currentCnt = 0;
+            }
+            maxCnt = Math.max(maxCnt, currentCnt);
+        }
+        System.out.println(maxCnt);
+    }
+
+
+    static class FastReader {
+
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
