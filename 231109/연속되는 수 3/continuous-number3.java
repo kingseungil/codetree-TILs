4e73dbb467dc68a826fb9d35c29017ca5572c7fa
Kingseungil <109774037+kingseungil@users.noreply.github.com>
2023-11-09 15:20:11 +0900

    Title: 연속되는 수 3, Time: 128ms, Memory: 9MB, Status: Passed - CodeTree

--- 231109/연속되는 수 3/continuous-number3.java
+++ 231109/연속되는 수 3/continuous-number3.java
@@ -19,7 +19,7 @@ public class Main {
             maxCnt = 1;
         }
         for (int i = 1; i < arr.length; i++) {
-            if ((arr[i] < 0 && arr[i - 1] < 0) || (arr[i] > 0 && arr[i - 1] > 0)) {
+            if (arr[i] * arr[i - 1] > 0) {
                 currentCnt++;
             } else {
                 currentCnt = 1;
