18045af82c1698349c060182b4aa01d98a05bf4b
Kingseungil <109774037+kingseungil@users.noreply.github.com>
2023-11-12 13:04:41 +0900

    Title: 선두를 지켜라 3, Time: 221ms, Memory: 27MB, Status: Passed - CodeTree

--- 231112/선두를 지켜라 3/keep-the-lead-3.java
+++ 231112/선두를 지켜라 3/keep-the-lead-3.java
@@ -1,56 +1,71 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    static int[] distancePerTime1 = new int[1_000_000];
-    static int[] distancePerTime2 = new int[1_000_000];
+    static int[] posA = new int[1_000_001];
+    static int[] posB = new int[1_000_001];
 
     public static void main(String[] args) {
         FastReader rd = new FastReader();
         int n = rd.nextInt();
         int m = rd.nextInt();
 
-        calcDistance(rd, n, distancePerTime1);
-        calcDistance(rd, m, distancePerTime2);
+        int timeA = recordLocation(rd, n, posA);
 
-        String lead = "none";
-        Map<Integer, String> map = new HashMap<>(); // {time, lead}
-        for (int i = 0; i < distancePerTime1.length; i++) {
-            if (distancePerTime1[i] > distancePerTime2[i]) {
-                lead = "A";
-            } else if (distancePerTime1[i] < distancePerTime2[i]) {
-                lead = "B";
-            } else if (distancePerTime1[i] == distancePerTime2[i]) {
-                lead = "both";
+        int timeB = recordLocation(rd, m, posB);
+
+        // A와 B 중 더 앞서 있는 경우를 확인합니다.
+        // A가 리더면 1, B가 리더면 2, 둘 다 리더면 3으로 관리합니다.
+        int leader = 0, ans = 0;
+        for (int i = 1; i < timeA; i++) {
+            if (posA[i] > posB[i]) {
+                // 조합이 바뀌었다면
+                // 답을 갱신합니다.
+                if (leader != 1) {
+                    ans++;
+                }
+
+                // 리더를 A로 변경합니다.
+                leader = 1;
+            } else if (posA[i] < posB[i]) {
+                // 조합이 바뀌었다면
+                // 답을 갱신합니다.
+                if (leader != 2) {
+                    ans++;
+                }
+
+                // 리더를 B로 변경합니다.
+                leader = 2;
+            } else {
+                // 조합이 바뀌었다면
+                // 답을 갱신합니다.
+                if (leader != 3) {
+                    ans++;
+                }
+
+                // 리더를 둘 다로 변경합니다.
+                leader = 3;
             }
-            map.put(i, lead);
         }
 
-        int count = 0;
-        // lead가 바뀔때마다 count 증가
-        for (int i = 0; i < distancePerTime1.length - 1; i++) {
-            if (!map.get(i).equals(map.get(i + 1))) {
-                count++;
-            }
-        }
-        System.out.println(count - 1); // 0초일때 lead가 바뀌는 것은 count하지 않음
+        System.out.print(ans);
     }
 
-    private static void calcDistance(FastReader rd, int n, int[] distancePerTime) {
-        int currentTime = 1;
+    private static int recordLocation(FastReader rd, int n, int[] pos) {
+        int time = 1;
         for (int i = 0; i < n; i++) {
             int v = rd.nextInt();
             int t = rd.nextInt();
-            for (int time = currentTime; time < currentTime + t; time++) {
-                distancePerTime[time] = distancePerTime[time - 1] + v;
+
+            while (t-- > 0) {
+                pos[time] = pos[time - 1] + v;
+                time++;
             }
-            currentTime = currentTime + t;
         }
+        return time;
     }
 
 
