2e2e3a40e12e43ed1202eaad561118e2e94720c3
Kingseungil <109774037+kingseungil@users.noreply.github.com>
2023-11-12 10:02:47 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 261ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 231112/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.java
@@ -0,0 +1,133 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.StringTokenizer;
+
+class Handshake {
+
+    int time;
+    int x;
+    int y;
+
+    Handshake(int time, int x, int y) {
+        this.time = time;
+        this.x = x;
+        this.y = y;
+    }
+}
+
+class Developer {
+
+    int idx;
+    int isInfect;
+    int infectionCount;
+
+    Developer(int idx) {
+        this.idx = idx;
+        this.isInfect = 0;
+        this.infectionCount = 0;
+    }
+
+    void infect() {
+        this.isInfect = 1;
+    }
+
+    boolean canInfect(int K) {
+        return this.isInfect == 1 && this.infectionCount < K;
+    }
+
+    void increaseInfectionCount() {
+        this.infectionCount++;
+    }
+}
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        FastReader rd = new FastReader();
+        int N = rd.nextInt(); // N명
+        int K = rd.nextInt(); // K번 악수동안만 전염병 옮김
+        int P = rd.nextInt(); // 처음 전염병 걸린 개발자 번호
+        int T = rd.nextInt(); // T번 악수
+
+        Developer[] developers = new Developer[N + 1];
+        for (int i = 1; i <= N; i++) {
+            developers[i] = new Developer(i);
+        }
+        developers[P].infect();
+
+        List<Handshake> handshakes = new ArrayList<>();
+        for (int i = 0; i < T; i++) {
+            int t = rd.nextInt(); // 시간
+            int x = rd.nextInt(); // 개발자 x
+            int y = rd.nextInt(); // 개발자 y
+
+            handshakes.add(new Handshake(t, x, y));
+        }
+
+        handshakes.sort(Comparator.comparingInt(h -> h.time));
+
+        for (Handshake handshake : handshakes) {
+            Developer x = developers[handshake.x];
+            Developer y = developers[handshake.y];
+            if (x.canInfect(K) || y.canInfect(K)) {
+                x.infect();
+                y.infect();
+                x.increaseInfectionCount();
+                y.increaseInfectionCount();
+            }
+        }
+
+        for (int i = 1; i <= N; i++) {
+            System.out.print(developers[i].isInfect);
+        }
+    }
+
+
+    static class FastReader {
+
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
