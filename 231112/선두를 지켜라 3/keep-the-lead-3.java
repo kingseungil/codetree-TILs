ba0090118eadd2a40d7a83532eae1aa2e97d9600
Kingseungil <109774037+kingseungil@users.noreply.github.com>
2023-11-12 12:59:14 +0900

    Title: 선두를 지켜라 3, Time: 1619ms, Memory: 120MB, Status: Passed - CodeTree

---
+++ 231112/선두를 지켜라 3/keep-the-lead-3.java
@@ -0,0 +1,99 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int[] distancePerTime1 = new int[1_000_000];
+    static int[] distancePerTime2 = new int[1_000_000];
+
+    public static void main(String[] args) {
+        FastReader rd = new FastReader();
+        int n = rd.nextInt();
+        int m = rd.nextInt();
+
+        calcDistance(rd, n, distancePerTime1);
+        calcDistance(rd, m, distancePerTime2);
+
+        String lead = "none";
+        Map<Integer, String> map = new HashMap<>(); // {time, lead}
+        for (int i = 0; i < distancePerTime1.length; i++) {
+            if (distancePerTime1[i] > distancePerTime2[i]) {
+                lead = "A";
+            } else if (distancePerTime1[i] < distancePerTime2[i]) {
+                lead = "B";
+            } else if (distancePerTime1[i] == distancePerTime2[i]) {
+                lead = "both";
+            }
+            map.put(i, lead);
+        }
+
+        int count = 0;
+        // lead가 바뀔때마다 count 증가
+        for (int i = 0; i < distancePerTime1.length - 1; i++) {
+            if (!map.get(i).equals(map.get(i + 1))) {
+                count++;
+            }
+        }
+        System.out.println(count - 1); // 0초일때 lead가 바뀌는 것은 count하지 않음
+    }
+
+    private static void calcDistance(FastReader rd, int n, int[] distancePerTime) {
+        int currentTime = 1;
+        for (int i = 0; i < n; i++) {
+            int v = rd.nextInt();
+            int t = rd.nextInt();
+            for (int time = currentTime; time < currentTime + t; time++) {
+                distancePerTime[time] = distancePerTime[time - 1] + v;
+            }
+            currentTime = currentTime + t;
+        }
+    }
+
+
+    static class FastReader {
+
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
