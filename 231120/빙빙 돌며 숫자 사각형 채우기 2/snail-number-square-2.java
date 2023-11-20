580065acb1fe6b5dd824bd8c603f069ca44c5504
Kingseungil <109774037+kingseungil@users.noreply.github.com>
2023-11-20 17:37:59 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기 2, Time: 280ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 231120/빙빙 돌며 숫자 사각형 채우기 2/snail-number-square-2.java
@@ -0,0 +1,95 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    // {동(E), 남(S), 서(W), 북(N)}
+    static int[] dx = new int[]{0, 1, 0, -1};
+    static int[] dy = new int[]{1, 0, -1, 0};
+    static int[][] grid;
+    static int n;
+    static int m;
+
+    // 시작점
+    static int x = 0;
+    static int y = 0;
+
+    // 시작 방향
+    static int dir = 1;
+
+    public static void main(String[] args) {
+        FastReader rd = new FastReader();
+        n = rd.nextInt();
+        m = rd.nextInt();
+        grid = new int[n][m];
+        grid[x][y] = 1;
+
+        for (int i = 2; i <= n * m; i++) {
+            int nx = x + dx[dir], ny = y + dy[dir];
+            if (!inRange(nx, ny) || grid[nx][ny] != 0) {
+                dir = (dir + 3) % 4;
+            }
+            x += dx[dir];
+            y += dy[dir];
+            grid[x][y] = i;
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(grid[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+
+    private static boolean inRange(int x, int y) {
+        return 0 <= x && x < n && 0 <= y && y < m;
+    }
+
+
+    static class FastReader {
+
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
