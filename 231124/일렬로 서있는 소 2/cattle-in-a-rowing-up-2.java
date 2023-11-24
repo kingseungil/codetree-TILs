2535a00f670434800e4597ef99af38c4d2840985
Kingseungil <109774037+kingseungil@users.noreply.github.com>
2023-11-24 10:35:33 +0900

    Title: 일렬로 서있는 소 2, Time: 98ms, Memory: 8MB, Status: Passed - CodeTree

--- 231124/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.java
+++ 231124/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.java
@@ -17,7 +17,7 @@ public class Main {
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
                 for (int k = j + 1; k < n; k++) {
-                    if (cows[i] < cows[j] && cows[i] < cows[k] && cows[j] < cows[k]) {
+                    if (cows[i] <= cows[j] && cows[i] <= cows[k] && cows[j] <= cows[k]) {
                         result++;
                     }
                 }
