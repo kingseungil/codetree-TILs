e5322fdaaac5a8603b4094bb147683ecb40271de
Kingseungil <109774037+kingseungil@users.noreply.github.com>
2023-11-24 10:31:07 +0900

    Title: 일렬로 서있는 소 2, Time: 97ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 231124/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.java
@@ -0,0 +1,72 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) {
+        FastReader rd = new FastReader();
+        int n = rd.nextInt();
+        int[] cows = new int[n];
+        for (int i = 0; i < n; i++) {
+            cows[i] = rd.nextInt();
+        }
+
+        int result = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    if (cows[i] < cows[j] && cows[i] < cows[k] && cows[j] < cows[k]) {
+                        result++;
+                    }
+                }
+            }
+        }
+        System.out.println(result);
+    }
+
+
+    static class FastReader {
+
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
