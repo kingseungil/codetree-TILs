f2777f94917c6b4be0599514bf4525eb30a44e51
Kingseungil <109774037+kingseungil@users.noreply.github.com>
2023-12-04 20:38:51 +0900

    Title: 특정 구간의 원소 평균값, Time: 107ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 231204/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.java
@@ -0,0 +1,83 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int n;
+    static int[] arr;
+
+    public static void main(String[] args) {
+        FastReader rd = new FastReader();
+        n = rd.nextInt();
+        arr = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            arr[i] = rd.nextInt();
+        }
+
+        int count = 0;
+        for (int i = 1; i <= n; i++) {
+            double avg = 0;
+            for (int j = i; j <= n; j++) {
+                int sum = 0;
+                for (int k = i; k <= j; k++) {
+                    sum += arr[k];
+                }
+                avg = (double) sum / (j - i + 1);
+                for (int k = i; k <= j; k++) {
+                    if (avg == arr[k]) {
+                        count++;
+                        break;
+                    }
+                }
+            }
+        }
+
+        System.out.println(count);
+    }
+
+
+    static class FastReader {
+
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
