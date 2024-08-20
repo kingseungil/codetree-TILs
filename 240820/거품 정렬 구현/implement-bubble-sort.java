6fa207f13baa15d71050fe3c40c0bc2777567c8a
Kingseungil <109774037+kingseungil@users.noreply.github.com>
2024-08-20 13:30:14 +0900

    Title: 거품 정렬 구현, Time: 101ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240820/거품 정렬 구현/implement-bubble-sort.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int n;
+    static ArrayList<Integer> list = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            n = Integer.parseInt(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            while (st.hasMoreTokens()) {
+                list.add(Integer.parseInt(st.nextToken()));
+            }
+
+            bubbleSort(list);
+
+            StringBuilder sb = new StringBuilder();
+            for (Integer item : list) {
+                sb.append(item).append(" ");
+            }
+            System.out.println(sb.toString().trim());
+        }
+    }
+
+    private static void bubbleSort(ArrayList<Integer> list) {
+        for (int i = 0; i < list.size() - 1; i++) {
+            for (int j = 0; j < list.size() - 1 - i; j++) {
+                if (list.get(j) > list.get(j + 1)) {
+                    int temp = list.get(j);
+                    list.set(j, list.get(j + 1));
+                    list.set(j + 1, temp);
+                }
+            }
+        }
+    }
+}
