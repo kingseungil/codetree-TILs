1185e222bf3bb74360b9c9229dd1454959548c64
Kingseungil <109774037+kingseungil@users.noreply.github.com>
2024-08-20 13:35:15 +0900

    Title: 거품 정렬 구현, Time: 110ms, Memory: 8MB, Status: Passed - CodeTree

--- 240820/거품 정렬 구현/implement-bubble-sort.java
+++ 240820/거품 정렬 구현/implement-bubble-sort.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -29,13 +30,26 @@ public class Main {
     }
 
     private static void bubbleSort(ArrayList<Integer> list) {
+//        for (int i = 0; i < list.size() - 1; i++) {
+//            for (int j = 0; j < list.size() - 1 - i; j++) {
+//                if (list.get(j) > list.get(j + 1)) {
+//                    int temp = list.get(j);
+//                    list.set(j, list.get(j + 1));
+//                    list.set(j + 1, temp);
+//                }
+//            }
+//        }
+        boolean swapped;
         for (int i = 0; i < list.size() - 1; i++) {
+            swapped = false;
             for (int j = 0; j < list.size() - 1 - i; j++) {
-                if (list.get(j) > list.get(j + 1)) {
-                    int temp = list.get(j);
-                    list.set(j, list.get(j + 1));
-                    list.set(j + 1, temp);
+                if (list.get(j).compareTo(list.get(j + 1)) > 0) {
+                    Collections.swap(list, j, j + 1);
+                    swapped = true;
                 }
+            }
+            if (!swapped) {
+                break;
             }
         }
     }
